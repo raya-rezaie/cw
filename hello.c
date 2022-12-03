a0e0b2b6765a2e3084738910e4329c9ad896e134
raya-rezaie <rayarezaie83@gmail.com>
2022-12-03 21:51:43 +0330

    Auto stash before revert of "02"

--- hello.c
+++ hello.c
@@ -4,6 +4,10 @@ int main()
 {
     printf("Hello World!\n");
 
+<<<<<<< Updated upstream
     for (int i=1;i<=10;i++)
+=======
+    for (int i=1;i<=122;i++)
+>>>>>>> Stashed changes
      printf("%d ",i);
 }
