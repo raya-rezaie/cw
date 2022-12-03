3882fd9ad33dfcbaae56bb02bfa9e97d634ba17d
raya-rezaie <119618572+raya-rezaie@users.noreply.github.com>
2022-12-03 23:30:09 +0330

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,13 +3,7 @@
 int main()
 {
     printf("Hello World!\n");
-<<<<<<< HEAD
-    for (int i=1;i<=10;i++)
     for (int i=1;i<=122;i++)
-=======
+    printf("%d ",i);
 
-    for (int i=1;i<=122;i++)
-
-     printf("%d ",i);
->>>>>>> parent of 28491ca... Update hello.c
 }
