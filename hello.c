1c91edff92fcbd845dfe7f1096dac17d54266a28
raya-rezaie <rayarezaie83@gmail.com>
2022-12-03 22:53:38 +0330

    Revert "Revert "Merge remote-tracking branch 'raya-rezaie/master'""
    This reverts commit d61cc85d3bd4dac982f4b9d4e90c59ca1d31abd8.

--- hello.c
+++ hello.c
@@ -3,11 +3,6 @@
 int main()
 {
     printf("Hello World!\n");
-
-<<<<<<< Updated upstream
     for (int i=1;i<=10;i++)
-=======
     for (int i=1;i<=122;i++)
->>>>>>> Stashed changes
-     printf("%d ",i);
 }
