394c8b0d25c7eb7e3ae8ca39654349f4c2d56cf9
raya-rezaie <119618572+raya-rezaie@users.noreply.github.com>
2022-12-03 23:06:12 +0330

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,14 +4,7 @@ int main()
 {
     printf("Hello World!\n");
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    for (int i=1;i<=10;i++)
-=======
     for (int i=1;i<=122;i++)
->>>>>>> Stashed changes
-=======
-    for (int i=1;i<=12;i++)
->>>>>>> master
+
      printf("%d ",i);
 }
